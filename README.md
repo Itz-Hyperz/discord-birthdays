87d78bd5e5be5ea9bbf9f41bfe8013c4860051f5
Itz-Hyperz <loganfgross@gmail.com>
2021-08-29 15:13:19 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,129 @@
 # discord-birthdays
 An NPM Module allowing you to setup birthday messages when it is a users birthday automatically!
+
+---
+
+## Support
+
+* [Discord](https://hyperz.dev/discord)
+
+* [Website](https://hyperz.dev/)
+
+---
+
+## Installation
+
+`npm i discord-birthdays@latest`
+
+---
+
+## Check SQL Table Status
+
+Check if the required SQL table(s) are imported
+
+| Entry        | Type | Definition | 
+|----------------|---------------|---------------|
+| #1   | SQL VAR  | Your SQL Connection Variable (con, db, etc)
+
+---
+
+## Initialization
+
+con, client, format, guildid, channels, header, footer, color
+
+The nodelogger function offers a fair amount of customization.
+
+| Entry        | Type | Definition | 
+|----------------|---------------|---------------|
+| #1   | SQL VAR     | Your SQL Connection Variable (con, db, etc)
+| #2   | CLIENT VAR  | Your bots "client" variable (client, bot, etc)
+| #3   | STRING      | The format you want bdays checked as (MM-DD-YYYY)
+| #4   | STRING      | The guild id for the birthday
+| #5   | ARRAY       | The channels to send the alert to
+| #6   | STRING      | The header of the birthday embed
+| #7   | STRING      | The footer of the birthday embed
+| #8   | STRING      | The color of the birthday embed
+
+---
+
+## Add Birthdays
+
+Add a birthday to the database function info
+
+| Entry        | Type | Definition | 
+|----------------|---------------|---------------|
+| #1   | SQL VAR  | Your SQL Connection Variable (con, db, etc)
+| #2   | STRING  | A User ID to add to the bdays table
+| #3   | STRING  | The date of the users birthday
+
+---
+
+## Remove Birthdays
+
+Remove a birthday from the database function info
+
+| Entry        | Type | Definition | 
+|----------------|---------------|---------------|
+| #1   | SQL VAR  | Your SQL Connection Variable (con, db, etc)
+| #2   | STRING  | A User ID to remove from the bdays table
+
+---
+
+## Code Example
+
+```js
+const Discord = require('discord.js')
+const client = new Discord.Client({
+    intents: ['GUILDS', 'GUILD_MESSAGES', "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING"],
+    partials: ["CHANNEL", "MESSAGE", "REACTIONS"],
+    allowedMentions: { parse: ['users', 'roles', 'everyone'], repliedUser: true }
+});
+
+let con;
+const mysql = require('mysql')
+client.utils = require('discord-birthdays')
+
+client.on(`ready`, () => {
+    con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "dbname"
+    });
+
+    console.log(`I AM READY WOOOOO`)
+
+    setTimeout(async () => {
+        await client.utils.dbcheck(con)
+        await client.utils.init(con, client, 'MM-DD-YYYY', '832721829822857296', ['879012702222168064'], `HAPPY BIRTHDAY`, `Happy BDay Lol`, `#ffffff`)
+    }, 3000)
+});
+
+client.on('messageCreate', message => {
+    let prefix = '!'
+    if (message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        let command = args.shift().toLowerCase();
+
+        if(command === 'ping') {
+            message.channel.send({ content: "pong!" })
+        } else if(command === 'add') {
+            let check = client.utils.addbday(con, message.author.id, args[0])
+            if(check) {
+                message.channel.send({ content: "Bday Added!" })
+            } else {
+                message.channel.send({ content: "Bday Add Failed!" })
+            }
+        } else if(command === 'remove') {
+            let check2 = client.utils.removebday(con, message.author.id)
+            if(check2) {
+                message.channel.send({ content: "Bday Removed!" })
+            } else {
+                message.channel.send({ content: "Failed to remove bday!" })
+            }
+        }
+    }
+});
+
+client.login(`YOUR_TOKEN_HERE`)
+```
---
+++ main.js
@@ -0,0 +1,103 @@
+// CREATED BY HYPERZ DEVELOPMENT ++ Hyperz#0001
+// CREATED BY HYPERZ DEVELOPMENT ++ Hyperz#0001
+// CREATED BY HYPERZ DEVELOPMENT ++ Hyperz#0001
+
+const moment = require('moment-timezone');
+const Discord = require('discord.js') // V13
+
+async function dbcheck(con) {
+    await con.query(`SELECT * FROM birthdays`, async (err, row) => {
+        if(err) {
+            await con.query(`CREATE TABLE birthdays ( userid TEXT, deDate TEXT );`, async (err, row) => {
+                if(err) {
+                    console.log(`Birthdays Issue Importing Table: `, err)
+                } else {
+                    console.log(`Imported Birthdays Table.`)
+                }
+            })
+        }
+    });
+}
+
+async function init(con, client, format, guildid, channels, header, footer, color) {
+    setInterval(async () => {
+        // Get current date
+        let datetime = moment().tz('America/New_York').format(format)
+        // Select all from table
+        await con.query(`SELECT * FROM birthdays`, async (err, rows) => {
+            if(err) throw err; // throw err
+            if(rows[0]) { // if there is any data
+                rows.forEach(async r => { // go through each bit of data
+                    if(r.deDate === datetime) { // If there birthday is equal to the current date
+                        let guild = await client.guilds.cache.get(guildid) // find guild id
+                        let user = await client.users.fetch(r.userid) // find user
+                        if(guild.members.cache.get(user.id)) { // if the member is in the server
+                            let bdayembed = new Discord.MessageEmbed() // create embed
+                            .setColor(color || `BLUE`)
+                            .setTitle(header || `🥳 Happy Birthday!`)
+                            .setThumbnail(user.avatarURL({ dynamic: true }) || `https://images.emojiterra.com/google/android-11/512px/1f389.png`)
+                            .setDescription(`It's **${user.tag} (<@${user.id}>)'s** Birthday today! Wish them a happy birthday and let's celebrate!\n**Date:** ${r.deDate}`)
+                            .setTimestamp()
+                            .setFooter(footer || 'Happy Birthday!')
+                            channels.forEach(async c => { // get each channel
+                                let thechannel = await client.channels.cache.get(c)
+                                if(thechannel !== undefined) {
+                                    thechannel.send({ embeds: [bdayembed] }).catch(e => {}) // send to each channel
+                                } else {
+                                    console.log(`A Birthday system channel ID is invalid...`) // log error
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+    }, 46800000) // Every 13 hours (avoids double pings)
+}
+
+async function addbday(con, userid, date) {
+    await con.query(`SELECT * FROM birthdays WHERE userid='${userid}'`, async (err, row) => {
+        if(err) {
+            console.log(`Failed to import birthday for userid ${userid} with date ${date}`);
+            return false;
+        }
+        if(!row[0]) {
+            await con.query(`INSERT INTO birthdays (userid, deDate) VALUES ("${userid}", "${date}")`, async (err, row) => {
+                if(err) {
+                    console.log(`Failed to import birthday for userid ${userid} with date ${date}`);
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+        }
+    });
+}
+
+async function removebday(con, userid) {
+    await con.query(`SELECT * FROM birthdays WHERE userid='${userid}'`, async (err, row) => {
+        if(err) {
+            console.log(`Failed to delete birthday for userid ${userid}`);
+            return false;
+        }
+        if(row[0]) {
+            await con.query(`DELETE FROM birthdays WHERE userid='${userid}'`, async (err, row) => {
+                if(err) {
+                    console.log(`Failed to delete birthday for userid ${userid}`);
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+        }
+    });
+}
+
+exports.dbcheck = dbcheck;
+exports.init = init;
+exports.addbday = addbday;
+exports.removebday = removebday;
+
+// CREATED BY HYPERZ DEVELOPMENT ++ Hyperz#0001
+// CREATED BY HYPERZ DEVELOPMENT ++ Hyperz#0001
+// CREATED BY HYPERZ DEVELOPMENT ++ Hyperz#0001
